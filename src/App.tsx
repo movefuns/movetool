3e850b9db486d338b1ef04146849e05347251676
wsc <386180127@qq.com>
2022-04-21 10:32:03 +0800

    fix index

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import Home from "./pages/Home/Home";
 function App() {
     return (
         <div className="App">
-
             <Home/>
         </div>
     );
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,15 +2,15 @@ import * as React from "react";
 
 import A from "../pages/A/A";
 import B from "../pages/B/B";
-import Home from "../pages/Home/Home";
 import Batch from "../pages/stc/Batch";
 import AddressInfo from "../pages/address/Info";
 import {Route, Routes} from "react-router-dom";
+import Index from "../pages/Index";
 
 
 export default function Router() {
     return (<Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Index/>}/>
         <Route path="/a" element={<A/>}/>
         <Route path="/b" element={<B/>}/>
         <Route path="/address/info" element={<AddressInfo/>}/>
