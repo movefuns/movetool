66cd92b17eaedfc743f9c2df17213ec09757e424
uvd <386180127@qq.com>
2022-07-08 21:43:51 +0800

    add batch convert token current only  usdt to stc (#35)
    * add batch convert token
    
    * add input type
    
    * add star

---
+++ src/pages/Stc/Batch/TransferConvert.tsx
@@ -0,0 +1,43 @@
+import {Card, CardContent, Grid, TextField} from "@mui/material";
+import * as React from 'react';
+import ConvertBatchTransfer from "./ConvertBatchTransfer"
+
+
+export default function TransferConvert() {
+    const [input, setInput] = React.useState("")
+
+    // filter address
+    const addressArray = input.split("\n").map(v => {
+        const items = v.split(/\s*,\s*|\s+/)
+        return {
+            address: items[0],
+            stc: items[1] ? items[1] : "0"
+        }
+    }).filter(v => v && v.address && v.address.startsWith("0x"))
+
+
+    return <><Grid container spacing={2}>
+        <Grid item xs={6} md={4}>
+            <Card sx={{minWidth: 275}}>
+                example:<br/>
+                0x0000000000000000001 2<br/>
+                0x0000000000000000001,2<br/>
+                <CardContent>
+                    <TextField
+                        id="filled-multiline-static"
+                        label="address"
+                        multiline
+                        rows={40}
+                        fullWidth
+                        onChange={(value) => setInput(value.target.value)}
+                        variant="filled"
+
+                    />
+                </CardContent>
+            </Card>
+        </Grid>
+        <Grid item xs={6} md={8}>
+            <ConvertBatchTransfer addressArray={addressArray}/>
+        </Grid>
+    </Grid></>
+}
