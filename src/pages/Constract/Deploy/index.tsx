5f210be2a81f4328cb05cee3fe5c791c9c699b08
Owen Wu <yubing744@163.com>
2022-08-16 23:31:16 +0800

    feat: opt label for deploy constract

--- src/pages/Constract/Deploy/index.tsx
+++ src/pages/Constract/Deploy/index.tsx
@@ -1,16 +1,14 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
+import Snackbar from '@mui/material/Snackbar';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { Alert, Stack, AlertColor } from "@mui/material";
 import { Dropzone, FileItem, FileValidated } from "@dropzone-ui/react";
-import { Alert, Stack, TextField, AlertColor } from "@mui/material";
-import Snackbar from '@mui/material/Snackbar';
 import { useState } from "react";
-import { transfer } from "../../../utils/stcWalletSdk";
-import { useParams } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-import { useAppSelector } from "../../../store/hooks";
 import { fileToBuffer } from '../../../utils/file';
 import { deployContract } from '../../../utils/stcWalletSdk';
 
@@ -18,19 +16,19 @@ export default function Donate() {
     const { t } = useTranslation();
 
     const [tipsType, setTipsType] = useState<AlertColor>("error")
-    const [errorTips, setErrorTips] = useState("")
-    const [openErrorTips, setOpenErrorTips] = useState(false);
+    const [tips, setTips] = useState("")
+    const [openTips, setOpenTips] = useState(false);
 
     const showSuccess = function (msg: string) {
         setTipsType("success")
-        setErrorTips(msg)
-        setOpenErrorTips(true)
+        setTips(msg)
+        setOpenTips(true)
     }
 
     const showError = function (msg: string) {
         setTipsType("error")
-        setErrorTips(msg)
-        setOpenErrorTips(true)
+        setTips(msg)
+        setOpenTips(true)
     }
 
     const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
@@ -38,7 +36,7 @@ export default function Donate() {
             return;
         }
 
-        setOpenErrorTips(false);
+        setOpenTips(false);
     };
 
     const [files, setFiles] = React.useState(new Array<FileValidated>());
@@ -61,13 +59,20 @@ export default function Donate() {
         } catch (err: any) {
             showError(t(err.message))
         }
+
+        // clear files
+        setFiles(new Array<FileValidated>());
     };
 
     return (
         <Card sx={{ minWidth: 275 }}>
             <CardContent>
                 <Stack spacing={2}>
-                    <Dropzone onChange={updateFiles} value={files}>
+                    <Typography variant="h5" component="div">
+                        {t("constract_deploy.title")}
+                    </Typography>
+
+                    <Dropzone onChange={updateFiles} value={files} maxFiles={1} accept=".blob" label={t("constract_deploy.drop_zone_label")}>
                         {files.map((file) => (
                             <FileItem {...file} preview />
                         ))}
@@ -75,13 +80,13 @@ export default function Donate() {
                 </Stack>
             </CardContent>
             <CardActions>
-                <Button variant="contained" fullWidth onClick={handleDeploy}>{t("menu.constract_deploy")}</Button>
+                <Button variant="contained" fullWidth onClick={handleDeploy}>{t("constract_deploy.deploy_btn")}</Button>
             </CardActions>
 
-            <Snackbar open={openErrorTips} autoHideDuration={6000} onClose={handleClose}
+            <Snackbar open={openTips} autoHideDuration={6000} onClose={handleClose}
                 anchorOrigin={{ vertical: 'top', horizontal: 'center' }}>
                 <Alert onClose={handleClose} severity={tipsType} sx={{ width: '100%' }}>
-                    {errorTips}
+                    {tips}
                 </Alert>
             </Snackbar>
         </Card>
